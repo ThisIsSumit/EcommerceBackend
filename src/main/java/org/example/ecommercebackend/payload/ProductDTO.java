0f7eab4e79fd4ce03554aca73c4edf588e362f30
ThisIsSumit <sumitkumar453827@gmail.com>
2025-02-20 16:40:37 +0530

    adding productDTO

---
+++ src/main/java/org/example/ecommercebackend/payload/ProductDTO.java
@@ -0,0 +1,18 @@
+package org.example.ecommercebackend.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDTO {
+    private Long productId;
+    private String productName;
+    private String image;
+    private Integer quantity;
+    private Double price;
+    private double discount;
+    private double  specialPrice;
+}
