5c3daf3c5aa6c023f8c802e11225778b819bf6df
ThisIsSumit <sumitkumar453827@gmail.com>
2025-03-12 18:01:22 +0530

    modifying user repo

--- src/main/java/org/example/ecommercebackend/repositories/UserRepository.java
+++ src/main/java/org/example/ecommercebackend/repositories/UserRepository.java
@@ -1,5 +1,8 @@
 package org.example.ecommercebackend.repositories;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.example.ecommercebackend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +13,9 @@ import java.util.Optional;
 public interface UserRepository  extends JpaRepository<User, Long> {
 
     Optional<User> findByUserName(String username);
+
+    Boolean existsByUserName(@NotBlank @Size(min = 3, max = 20) String username);
+
+    Boolean existsByEmail(@NotBlank @Size(max = 50) @Email String email);
 }
 
