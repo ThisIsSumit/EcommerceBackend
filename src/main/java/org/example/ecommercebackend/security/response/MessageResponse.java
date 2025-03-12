c2594e6693d26faf09b6133a14b46334685e01b1
ThisIsSumit <sumitkumar453827@gmail.com>
2025-03-12 18:00:29 +0530

    adding MessageResponse

---
+++ src/main/java/org/example/ecommercebackend/security/response/MessageResponse.java
@@ -0,0 +1,17 @@
+package org.example.ecommercebackend.security.response;
+
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
