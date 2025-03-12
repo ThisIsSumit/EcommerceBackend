5175738ada70e97cef1fdf1ea8bb83a010eeb2db
ThisIsSumit <sumitkumar453827@gmail.com>
2025-03-12 18:01:41 +0530

    adding signup method

--- src/main/java/org/example/ecommercebackend/controller/AuthController.java
+++ src/main/java/org/example/ecommercebackend/controller/AuthController.java
@@ -1,10 +1,19 @@
 package org.example.ecommercebackend.controller;
 
+import jakarta.validation.Valid;
+import org.example.ecommercebackend.entities.AppRole;
+import org.example.ecommercebackend.entities.Role;
+import org.example.ecommercebackend.entities.User;
+import org.example.ecommercebackend.repositories.RoleRepository;
+import org.example.ecommercebackend.repositories.UserRepository;
 import org.example.ecommercebackend.security.jwt.JwtUtils;
 import org.example.ecommercebackend.security.request.LoginRequest;
+import org.example.ecommercebackend.security.request.SignupRequest;
+import org.example.ecommercebackend.security.response.MessageResponse;
 import org.example.ecommercebackend.security.response.UserInfoResponse;
 import org.example.ecommercebackend.security.services.UserDetailsImpl;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +29,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
@@ -31,6 +37,17 @@ public class AuthController {
     @Autowired
     private JwtUtils jwtUtils;
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest)
@@ -54,5 +71,56 @@ public class AuthController {
         UserInfoResponse response= new UserInfoResponse(userDetails.getId(), userDetails.getUsername(),jwtToken,roles);
         return ResponseEntity.ok(response);
 
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        if (userRepository.existsByUserName(signUpRequest.getUsername())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    case "seller":
+                        Role modRole = roleRepository.findByRoleName(AppRole.ROLE_SELLER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(modRole);
+
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
     }
 }
