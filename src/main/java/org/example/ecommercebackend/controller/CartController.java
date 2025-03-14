77b98f621613c1db23733c3184d1b2a26ee707d4
ThisIsSumit <sumitkumar453827@gmail.com>
2025-03-14 18:37:12 +0530

    adding cart controller and auth util file

---
+++ src/main/java/org/example/ecommercebackend/controller/CartController.java
@@ -0,0 +1,26 @@
+package org.example.ecommercebackend.controller;
+
+import org.example.ecommercebackend.payload.CartDTO;
+import org.example.ecommercebackend.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping("/carts/products/{productId}/quantity/{quantity}")
+    public ResponseEntity<CartDTO> addProductToCart(@PathVariable Long productId,
+                                                    @PathVariable Integer quantity){
+        CartDTO cartDTO = cartService.addProductToCart(productId, quantity);
+        return new ResponseEntity<>(cartDTO, HttpStatus.CREATED);
+    }
+}
