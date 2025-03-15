0578dae8065deade5839a974ce9ebbef8f1ba753
ThisIsSumit <sumitkumar453827@gmail.com>
2025-03-15 18:50:49 +0530

    adding query to cart and cartitem repo

--- src/main/java/org/example/ecommercebackend/repositories/CartRepository.java
+++ src/main/java/org/example/ecommercebackend/repositories/CartRepository.java
@@ -1,14 +1,16 @@
 package org.example.ecommercebackend.repositories;
 
+
 import org.example.ecommercebackend.entities.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartRepository extends JpaRepository<Cart,Long> {
-    @Query("SELECT c FROM Cart c WHERE c.user.email=?1")
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    @Query("SELECT c FROM Cart c WHERE c.user.email = ?1")
     Cart findCartByEmail(String email);
 
+    @Query("SELECT c FROM Cart c WHERE c.user.email = ?1 AND c.cartId = ?2")
     Cart findCartByEmailAndCartId(String emailId, Long cartId);
 }
